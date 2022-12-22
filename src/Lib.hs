6c99e7a93527afb0c7c08e7ac3ea714cdcc9b2f8
Lucas G <lucas@11xx.org>
2022-12-22 05:15:06 -0300

    Refactor and semi-working
    - missing starting to count higher than 1
    - missing some other core functionality

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,24 +9,85 @@ import System.FilePath.Posix
 copyCheck :: IO ()
 copyCheck = do
   opts <- execParser optsParserInfo
-  putStrLn =<< run opts 1
+  f <- run opts 1
+  putStrLn f
 
 -- run :: (Show p, Num p) => Opts -> p -> IO [Char]
 run :: Opts -> Int -> IO String
 run opts@(Opts f t d p) n = do
-  -- same as `run (Opts f t d p) n = do'
-  let filename = takeFileName f
-      filepath = d </> filename
-      -- ren = filepath ++ t ++ pad n p
-  doesExist <- doesFileExist filepath
+  let he = hasExt opts
+      ih = isHidden opts
+      hc = hasCopyText opts n
+      dir = getDir opts
 
-  if doesExist
-    then do doesExist' <- doesFileExist $ filepath ++ t ++ pad n p
-            if doesExist'
-              then run opts (n + 1)
-              else pure $ filepath ++ t ++ pad n p
-    else pure filename
+  let isHiddenNoExt
+        | he && ih = False
+        | otherwise = True
 
+  let ren = replace fa ta tb
+        where
+          fa = takeFileName f
+          ta = copyText opts n
+          tb = copyText opts $ n + 1
+
+  reb <- doesFileExist ren
+
+  let renamed n'
+        | he           = replace ct "" (takeBaseName f) ++ ct ++ takeExtension f
+        | ih && not he = replace ct "" (takeFileName f) ++ ct
+        | otherwise    = replace ct "" (takeBaseName f) ++ ct ++ takeExtension f
+        where
+          ct = copyText opts n'
+
+  let renamedPath = getDir opts </> renamed n
+
+  renamedE <- doesFileExist renamedPath
+
+  recur <- if hasCopyText opts n && reb || renamedE
+           then run opts (n + 1)
+           else pure ""
+
+  let doit
+        | n > 1000 = error "ERROR: Maximum recursion depth reached."
+        | hasCopyText opts n = if reb
+                               then recur
+                               else ren
+        | renamedE = recur
+        | otherwise = renamed n
+
+  pure doit
+
+hasCopyText opts@(Opts f _ _ _) n
+  | copyText opts n `isInfixOf` takeFileName f = True
+  | otherwise = False
+
+copyText (Opts _ t _ p) n = t ++ pad n p
+
+
+hasExt :: Opts -> Bool
+hasExt (Opts f _ _ _)
+  | head (takeFileName f) == '.' && takeExtension f == takeFileName f = False
+  | hasExtension f = True
+  | otherwise = False
+
+isHidden (Opts f _ _ _)
+  | head (takeFileName f) == '.' = True
+  | otherwise = False
+
+-- same as getPath but without filename
+getDir (Opts f _ d _)
+  | takeDirectory f == d = d
+  | d == "." && takeDirectory f /= d = takeDirectory f
+  | otherwise = d
+
+getPath :: Opts -> FilePath
+getPath (Opts f _ d _)
+  | takeDirectory f == d = d </> takeFileName f
+  | d == "." && takeDirectory f /= d = takeDirectory f </> takeFileName f
+  | otherwise = d </> takeFileName f
+
+-- rename (Opts f t d p) n
+--   | t ++ pad n d `isInfixOf` takeFileName f =
 
 pad :: Show p => p -> Int -> [Char]
 pad n p = replicate (p - length sn) '0' <> sn
